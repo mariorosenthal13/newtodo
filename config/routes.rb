7f5df456db364ed99465aa1254205a37ed8d8e13
Mario Rosenthal <mario.f.rosenthal@gmail.com>
2017-02-10 11:16:17 -0500

    new home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :todo_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get '/' 
+
+  root 'todo_items#index'
+
+
 end
